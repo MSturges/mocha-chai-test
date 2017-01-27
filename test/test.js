18c4cebd71631de0e049d7c8c1731bdc3d2686c1
MSturges <Sturges3232@gmail.com>
2017-01-27 08:30:15 -0700

    init

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var code = require('../main');
+var expect = require('chai').expect;
+
+
+// describe defines a test suite. it defines a single test
+//
+// A suite takes a string that describes what a particular suite is testing.
+// 
+// A test takes a string that describes the function
+//
+// Test!
+
+
+
+describe("Hello World", function() {
+  it("should say 'Hello, World!' when ran");
+});
